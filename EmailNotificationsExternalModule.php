8d63e16b090514079d0e1f9c5a850521c5953da1
maxramirez84 <maxramirez84@gmail.com>
2018-12-10 16:55:57 +0100

    Basic initial files required by the External Modules REDCap Framework.

---
+++ EmailNotificationsExternalModule.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maramirez
+ * Date: 10/12/18
+ * Time: 16:38
+ */
+
+namespace ISGlobal\EmailNotificationsExternalModule;
+
+
+class EmailNotificationsExternalModule extends \ExternalModules\AbstractExternalModule
+{
+
+}
